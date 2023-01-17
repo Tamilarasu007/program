5eeb27e1f8f35418211eabefe67d09762446df8d
kingtamil75 <122734505+kingtamil75@users.noreply.github.com>
2023-01-17 20:56:36 +0530

    Update print.py

--- print.py
+++ print.py
@@ -1,7 +1,7 @@
 
 r=float(input("Input the radius of the circle:"))
 area = (22/7)*r*r
-print("the area of the circle is:")
+print("the area of the circle is:",area)
 ============================================================================
 import os
 filename=input("enter filename: ")
